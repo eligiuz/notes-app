634f494a5b05e85a6af6823f32c8df116111abce
Eligio Cachón Menéndez <ecachon@gmail.com>
2020-03-06 18:54:11 -0600

    CRUD con problemas en modificar datos

--- src/routes/notes.js
+++ src/routes/notes.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const router = express.Router();
 
+const Note = require('../models/Note');
+
 router.get('/notes/add', (req, res) => {
     res.render('notes/new-note');
 });
 
-router.post('/notes/new-note', (req, res) => {
+router.post('/notes/new-note', async (req, res) => {
     const { title, description} = req.body;
     const errors = [];
     if (!title){
@@ -21,12 +23,44 @@ router.post('/notes/new-note', (req, res) => {
             description
         });
     } else {
-        res.send('ok');
+        const newNote = new Note({title, description});
+        await newNote.save();
+        res.redirect('/notes');
     }
 });
 
-router.get('/notes', (req, res) => {
-    res.send('Notas de la base de datos');
+router.get('/notes', async (req, res) => {
+    //const notes = await Note.find();
+    await Note.find().sort({date: 'desc'})
+        .then(documentos => {
+            const contexto = {
+                notes: documentos.map(documento => {
+                    return{
+                        id: documento._id,
+                        title: documento.title,
+                        description: documento.description
+                    }
+                })
+            };
+            res.render('notes/all-notes', {notes:contexto.notes});
+        })
+});
+
+router.get('/notes/edit/:id', async (req, res) => {
+    //const note = await Note.findById(req.params.id);
+    await Note.findById(req.params.id)
+        .then(documentos1 => {
+            const contexto1 = {
+                note: documentos1.map(documento1 => {
+                    return{
+                        id: documento1._id,
+                        title:documento1.title,
+                        description: documento1.description
+                    }
+                })
+            };
+            res.render('notes/edit-note', {note:contexto1.note});
+        })
 });
 
 module.exports = router;
